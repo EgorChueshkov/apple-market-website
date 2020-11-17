dc4567a757c01cd9e56942576f0de102b56c127c
EgorChueshkov <egor.chueshkov@yandex.com>
2020-11-18 00:56:33 +0300

    add templates

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,30 +1,34 @@
-from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.forms import ModelChoiceField
 from django.contrib import admin
-
 from .models import *
-from PIL import Image
 
 
-class NotebookAdminForm(ModelForm):
+# class NotebookAdminForm(ModelForm):
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['image'].help_text = mark_safe(
+#             '''<span style="color:red; font-size:14px;">При загрузке изображения с разрешением больше {}x{} оно будет обрезано!</span>
+#             '''.format(
+#             *Product.MAX_RESOLUTION))
 
-    MIN_RESOLUTION = (400, 400)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = 'Загружайте изображения с минимальным разрешением {}x{}'.format(*self.MIN_RESOLUTION)
-
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение изображения меньше минимального!')
-        return image
+    # def clean_image(self):
+    #     image = self.cleaned_data['image']
+    #     img = Image.open(image)
+    #     min_height, min_width = Product.MIN_RESOLUTION
+    #     max_height, max_width = Product.MAX_RESOLUTION
+    #     if image.size > Product.MAX_IMAGE_SIZE:
+    #         raise ValidationError('Размер изображения не должен превышать 3МВ!')
+    #     if img.height < min_height or img.width < min_width:
+    #         raise ValidationError('Разрешение изображения меньше минимального!')
+    #     if img.height > max_height or img.width > max_width:
+    #         raise ValidationError('Разрешение изображения больше максимального!')
+    #     return image
 
 
 class NotebookAdmin(admin.ModelAdmin):
 
-    form = NotebookAdminForm
+    #form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -2,8 +2,22 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.urls import reverse
 
 User = get_user_model()
+
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__._meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
+
+
+class MinResolutionErrorException(Exception):
+    pass
+
+
+class MaxResolutionErrorException(Exception):
+    pass
 
 
 class LatestProductsManager:
@@ -40,6 +54,10 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (800, 800)
+    MAX_IMAGE_SIZE = 3145728
+
     class Meta:
         abstract = True
 
@@ -53,6 +71,28 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    # def save(self, *args, **kwargs):
+    #     # image = self.image
+    #     # img = Image.open(image)
+    #     # min_height, min_width = self.MIN_RESOLUTION
+    #     # max_height, max_width = self.MAX_RESOLUTION
+    #     # if img.height < min_height or img.width < min_width:
+    #     #     raise MinResolutionErrorException('Разрешение изображения меньше минимального!')
+    #     # if img.height > max_height or img.width > max_width:
+    #     #     raise MaxResolutionErrorException('Разрешение изображения больше максимального!')
+    #     image = self.image
+    #     img = Image.open(image)
+    #     new_img = img.convert('RGB')
+    #     resize_new_image = new_img.resize((200, 200), Image.ANTIALIAS)
+    #     filestream = BytesIO()
+    #     resize_new_image.save(filestream, 'JPEG', quality=90)
+    #     filestream.seek(0)
+    #     name = '{}.{}'.format(*self.image.name.split('.'))
+    #     self.image = InMemoryUploadedFile(
+    #         filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(filestream), None
+    #     )
+    #     super().save(*args, **kwargs)
+
 
 class Notebook(Product):
 
@@ -65,6 +105,9 @@ class Notebook(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class Smartphone(Product):
@@ -81,6 +124,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
