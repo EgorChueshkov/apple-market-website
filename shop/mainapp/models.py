a73138f162c9bb39dab05cdfdeb0aecb60bf5a8d
EgorChueshkov <egor.chueshkov@yandex.com>
2020-11-19 13:57:38 +0300

    add delete items and change items in cart

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -96,6 +96,9 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    def get_model_name(self):
+        return self.__class__.__name__.lower()
+
     # def save(self, *args, **kwargs):
     #     # image = self.image
     #     # img = Image.open(image)
@@ -189,6 +192,15 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    def save(self, *args, **kwargs):
+        cart_data = self.products.aggregate(models.Sum('final_price'), models.Count('id'))
+        if cart_data.get('final_price__sum'):
+            self.final_price = cart_data['final_price__sum']
+        else:
+            self.final_price = 0
+        self.total_products = cart_data['id__count']
+        super().save(*args, **kwargs)
 
 
 class Customer(models.Model):
